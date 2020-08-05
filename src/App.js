90474c1adacda7a70a52d2ea38bb742e4d7d5c66
lhk3337 <lhk3337@naver.com>
2020-08-05 14:54:10 +0900

    react route setting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React from "react";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello enjoy Coding!!</h1>
-      <h2>Hellow world</h2>
-    </div>
+    <Router>
+      <div className="app">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/checkout">Checkout</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Router path="/login">
+            <h1>Login page</h1>
+          </Router>
+          <Route exact path="/">
+            <h1>Home Page!!!</h1>
+          </Route>
+          <Route path="/">
+            <h1>Not found</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
